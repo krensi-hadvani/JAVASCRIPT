dc261e6e08e17014ddf4a9b9594a4fbb588d5e12
krensi-hadvani <164131955+krensi-hadvani@users.noreply.github.com>
2025-07-04 06:04:36 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144513
+let accountEmail = "krensi@google.com"
+var accountPassword = "123456"
+accountCity ="Keshod"
+
+//accountId = 2
+accountEmail = "hkrensi@gmail.com"
+accountPassword = "456789"
+accountCity = "Junagadh"
+
+console.table ([accountId , accountEmail , accountPassword , accountCity])
