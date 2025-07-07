079304ffe233cb62eff3cc7370a76e5064cb781c
krensi-hadvani <164131955+krensi-hadvani@users.noreply.github.com>
2025-07-07 06:14:57 +0000

    container config done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,10 +2,20 @@ const accountId = 144513
 let accountEmail = "krensi@google.com"
 var accountPassword = "123456"
 accountCity ="Keshod"
+let accounState
 
-//accountId = 2
+//accountId = 2 not allowed
 accountEmail = "hkrensi@gmail.com"
 accountPassword = "456789"
 accountCity = "Junagadh"
 
-console.table ([accountId , accountEmail , accountPassword , accountCity])
+
+
+console.log(accountId)
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table ([accountId , accountEmail , accountPassword , accountCity, accounState])
