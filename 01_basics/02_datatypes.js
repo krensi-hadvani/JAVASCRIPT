a39becfcaa3177ea881d261ccc04ff02f92944fd
krensi-hadvani <164131955+krensi-hadvani@users.noreply.github.com>
2025-07-13 05:44:10 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -6,3 +6,27 @@ console.log(3
     + 3) //code readability
 
 console.log("Krensi")    
+
+
+
+let name = "krensi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => stanalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "krensi")
+console.log(typeof age)
+console.log(typeof undefined); //undefined
+console.log(typeof null); //objet
