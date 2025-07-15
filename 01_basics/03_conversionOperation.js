38977716438875d4e666eb64d7f85d3eddeb8750
krensi-hadvani <164131955+krensi-hadvani@users.noreply.github.com>
2025-07-15 05:15:08 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,43 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console.log (stringNumber);
 console.log (typeof stringNumber);
+
+//*************************operation***************************** 
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Krensi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
